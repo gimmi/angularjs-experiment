cfe5532d30bad5d4fcfbd9b6ac7cf4f74a4aa490
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-05-01 11:56:07 +0200

    Initial commit

---
+++ client/app/js/filters.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('myApp.filters', []).filter('interpolate', ['version', function(version) {
+	return function(text) {
+		return String(text).replace(/\%VERSION\%/mg, version);
+	}
+}]);
